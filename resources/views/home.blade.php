d38bd6130cfe5db5b8bcfe8dc9cef48500c2281a
montderakeel <montder.akeel101@gmail.com>
2019-02-07 17:49:46 +0300

    edit home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Welcome To Dashboard</div>
+                <div class="card-header">The Dashboard</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    You are logged in as {{ Auth::user()->name }}
                 </div>
             </div>
         </div>
